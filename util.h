17cfca0ce4e10f76ed2b6ed7b712a76ea2b446ef
Luciano Barreira <meckmao@gmail.com>
2016-03-09 15:49:50 -0300

    Constants merged into header

---
+++ util.h
@@ -0,0 +1,12 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+const float crossover_rate = 0.8;
+const float mutation_rate = 0.007; // that's a high mutation rate compared to real rates.
+const unsigned int POP_SIZE = 50;
+const unsigned int chromo_length = 100;
+const unsigned int gene_length = 4;
+const unsigned int max_allowable_generations = 1000;
+const unsigned int BUFF_MAX = 256;
+
+#endif
